5d3b4d367bb752265f2016d4126c7fae08b27cb1
Rare Raptor <connorwtuncc@gmail.com>
2022-06-01 21:53:51 -0400

    made some progress with getting city and temp to display on page

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
-var textInput = document.querySelector('.text')
+var textInputEl = document.querySelector('.text')
+var buttonEl = document.querySelector('.searchBtn')
+var nameEl = document.querySelector('.name')
+var descriptionEl = document.querySelector('.desc')
+var tempEl = document.querySelector('.temp')
 
-function getApi() {
-
-var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+textInput.value+'&appid=0cf1c7e9555bfeee92b38ab834c53129'
-
-fetch(requestURL)
-    .then(function(response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        getOneCallWeather(data[0].lat, data[0].lon);
-    });
+function handleSubmit(event) {
+    event.preventDefault()
+    var city = textInputEl.value;
+    if (city) {
+        getApi(city)
+        textInputEl.value = '';
+    } else {
+        alert('please enter a city name')
+    }
 }
 
-function getOneCallWeather(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=0cf1c7e9555bfeee92b38ab834c53129`)
-        .then(function(response) {
-            return response.json()
+function getApi(city) {
+var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=0cf1c7e9555bfeee92b38ab834c53129`;
+
+    fetch(requestURL)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
         })
-        .then(function(data) {
-            console.log(data)
-        })
+        .then(function (data) {
+            console.log(data);
+            displayWeather(data, city);
+            //getOneCallWeather(data[0].lat, data[0].lon);
+        });
+};
+
+var displayWeather = function(weather, searchedCity) {
+    nameEl.textContent = searchedCity;
+    tempEl.textContent = "Temperature: " + weather.main.temp;
 }
+
+
+// function getOneCallWeather(lat, lon) {
+//     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=0cf1c7e9555bfeee92b38ab834c53129`)
+//         .then(function (response) {
+//             console.log(response)
+//             return response.json()
+//         })
+//         .then(function (data) {
+//             console.log(data)
+//         })
+// }
+buttonEl.addEventListener('click', handleSubmit);
+
 getApi()
