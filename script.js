8f66ff7aca26d32c335bab1292e1f61d09714072
Rare Raptor <connorwtuncc@gmail.com>
2022-06-02 18:43:13 -0400

    formatting and trying out functions

--- script.js
+++ script.js
@@ -14,73 +14,75 @@ var items = JSON.parse(localStorage.getItem('items')) || [];
 
 function init() {
 
-//displays weather on page
-function handleSubmit(city) {
-    var city = textInputEl.value;
-    if (city) {
-        getApi(city)
-        textInputEl.value = '';
-    } 
-    //else {
-    //     alert('please enter a city name');
-    // }
-    //storeItem();
-}
-
-var displayWeatherButtons = function () {
-    var items = JSON.parse(localStorage.getItem('items')) || [];
-    for (var searches of items) {
-        var btnEl = document.createElement('button');
-        btnEl.dataset.item = searches;
-        btnEl.setAttribute('class', 'd-block')
-        btnEl.className = 'btn'
-        btnEl.textContent = searches;
-        searchBtnEl.appendChild(btnEl)
+    //displays weather on page
+    function handleSubmit(city) {
+        var city = textInputEl.value;
+        if (city) {
+            getApi(city)
+            textInputEl.value = '';
+        }
+        //else {
+        //     alert('please enter a city name');
+        // }
+        //storeItem();
     }
-}
 
-function saveData () {
-    for (var i = 0; i < items.length; i++) {
-        console.log(items)
+    var displayWeatherButtons = function () {
+        var items = JSON.parse(localStorage.getItem('items')) || [];
+        for (var searches of items) {
+            var btnEl = document.createElement('button');
+            btnEl.dataset.item = searches;
+            btnEl.setAttribute('class', 'd-block')
+            btnEl.className = 'btn'
+            btnEl.textContent = searches;
+            searchBtnEl.appendChild(btnEl)
+        }
     }
+
+    function saveData() {
+        for (var i = 0; i < items.length; i++) {
+            searches.value = items++;
+            console.log(items)
+        }
+    }
+
+    //gets api data for displaying weather
+    function getApi(city) {
+        var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=0cf1c7e9555bfeee92b38ab834c53129`;
+
+        fetch(requestURL)
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                displayWeather(data, city);
+            });
+    };
+
+    //grabs weather data from array
+    var displayWeather = function (weather, searchedCity) {
+        nameEl.textContent = searchedCity;
+        tempEl.textContent = "Temperature: " + weather.main.temp;
+        windEl.textContent = "Wind Speed: " + weather.wind.speed;
+        humidEl.textContent = "Humidity: " + weather.main.humidity;
+        uvEl.textContent = "";
+    }
+
+    //saves to local storage
+    buttonEl.addEventListener('click', function () {
+        var searches = textInputEl.value;
+        handleSubmit(searches);
+        //console.log(searches)
+        var items = JSON.parse(localStorage.getItem(searches)) || [];
+        items.push(searches);
+        localStorage.setItem('items', JSON.stringify(items));
+        displayWeatherButtons();
+        saveData();
+    });
+    //buttonEl.addEventListener('click', handleSubmit)
 }
 
-//gets api data for displaying weather
-function getApi(city) {
-    var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=0cf1c7e9555bfeee92b38ab834c53129`;
-
-    fetch(requestURL)
-        .then(function (response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            displayWeather(data, city);
-        });
-};
-
-//grabs weather data from array
-var displayWeather = function (weather, searchedCity) {
-    nameEl.textContent = searchedCity;
-    tempEl.textContent = "Temperature: " + weather.main.temp;
-    windEl.textContent = "Wind Speed: " + weather.wind.speed;
-    humidEl.textContent = "Humidity: " + weather.main.humidity;
-    uvEl.textContent = "";
-}
-
-//saves to local storage
-buttonEl.addEventListener('click', function() {
-    var searches = textInputEl.value;
-    handleSubmit(searches);
-    //console.log(searches)
-    var items = JSON.parse(localStorage.getItem(searches)) || [];
-    items.push(searches);
-    localStorage.setItem('items', JSON.stringify(items));
-    displayWeatherButtons();
-    saveData();
-});
-//buttonEl.addEventListener('click', handleSubmit)
-}
 init();
 
