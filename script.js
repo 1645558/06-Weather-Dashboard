a2010b6df4ba95ff8f60e6a3faa9404b9914575d
Rare Raptor <connorwtuncc@gmail.com>
2022-06-02 16:57:32 -0400

    fixed local storage, works as it should now

--- script.js
+++ script.js
@@ -3,23 +3,40 @@ var buttonEl = document.querySelector('.searchBtn');
 var nameEl = document.querySelector('.name');
 var descriptionEl = document.querySelector('.desc');
 var tempEl = document.querySelector('.temp');
-windEl = document.querySelector('.wind');
+var windEl = document.querySelector('.wind');
 var humidEl = document.querySelector('.humidity')
 var uvEl = document.querySelector('.uv');
+var searchBtnEl = document.querySelector('.search-buttons');
+// var items = JSON.parse(localStorage.getItem('searchBtn')) || [];
+// const histEl = document.getElementById('history');
 
-function handleSubmit(event) {
-    event.preventDefault()
+function init() {
+
+function handleSubmit(city) {
     var city = textInputEl.value;
     if (city) {
         getApi(city)
         textInputEl.value = '';
-    } else {
-        alert('please enter a city name');
-    }
+    } 
+    //else {
+    //     alert('please enter a city name');
+    // }
+    //storeItem();
 }
 
+// var displayWeatherButtons = function () {
+//     var items = JSON.parse(localStorage.getItem('items')) || [];
+//     for (var item of items) {
+//         var btnEl = document.createElement('button');
+//         btnEl.dataset.item = item;
+//         btnEl.className = 'btn'
+//         btnEl.textContent = item;
+//         searchBtnEl.appendChild(btnEl)
+//     }
+// }
+
 function getApi(city) {
-var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=0cf1c7e9555bfeee92b38ab834c53129`;
+    var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=0cf1c7e9555bfeee92b38ab834c53129`;
 
     fetch(requestURL)
         .then(function (response) {
@@ -29,11 +46,10 @@ var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&unit
         .then(function (data) {
             console.log(data);
             displayWeather(data, city);
-            //getOneCallWeather(data[0].lat, data[0].lon);
         });
 };
 
-var displayWeather = function(weather, searchedCity) {
+var displayWeather = function (weather, searchedCity) {
     nameEl.textContent = searchedCity;
     tempEl.textContent = "Temperature: " + weather.main.temp;
     windEl.textContent = "Wind Speed: " + weather.wind.speed;
@@ -41,15 +57,20 @@ var displayWeather = function(weather, searchedCity) {
     uvEl.textContent = "";
 }
 
+buttonEl.addEventListener('click', function() {
+    var searches = textInputEl.value;
+    handleSubmit(searches);
+    console.log(searches)
+    var items = JSON.parse(localStorage.getItem('searchBtn')) || [];
+    items.push(searches);
+    localStorage.setItem('items', JSON.stringify(items));
+})
+//buttonEl.addEventListener('click', handleSubmit);
 
-// function getOneCallWeather(lat, lon) {
-//     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=0cf1c7e9555bfeee92b38ab834c53129`)
-//         .then(function (response) {
-//             console.log(response)
-//             return response.json()
-//         })
-//         .then(function (data) {
-//             console.log(data)
-//         })
-// }
-buttonEl.addEventListener('click', handleSubmit);
+}
+
+init();
+
+
+
+
