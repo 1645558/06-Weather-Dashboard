990012f21f10e224ac9cc7916586a6b15ef992c7
Rare Raptor <connorwtuncc@gmail.com>
2022-06-02 22:48:32 -0400

    got api for weather and attempting local storage again

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 var cities = [];
 
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    
+
+var saveSearch = function() {
+    localStorage.setItem('cities', JSON.stringify(cities));
 }
+
+var getCityWeather = function(city){
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=0cf1c7e9555bfeee92b38ab834c53129`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+        });
+    });
+};
 
