d131b63ca270c6edae236bc0e0bf311d73234f2c
Rare Raptor <connorwtuncc@gmail.com>
2022-06-02 22:40:50 -0400

    did html, now starting java over again

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+var cities = [];
 
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+}
 
