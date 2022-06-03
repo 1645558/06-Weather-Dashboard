5d065fde7357b211ea69b4a4b4eb9a379ce4efbe
Rare Raptor <connorwtuncc@gmail.com>
2022-06-02 22:58:19 -0400

    added function for grabbing weather data

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-var cityFormEl=document.querySelector("#city-search-form");
-var cityInputEl=document.querySelector("#city");
-var weatherContainerEl=document.querySelector("#current-weather-container");
+var cityFormEl = document.querySelector("#city-search-form");
+var cityInputEl = document.querySelector("#city");
+var weatherContainerEl = document.querySelector("#current-weather-container");
 var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 var cities = [];
 
-
-var saveSearch = function() {
+//localstorage save
+var saveSearch = function () {
     localStorage.setItem('cities', JSON.stringify(cities));
 }
 
-var getCityWeather = function(city){
+//grab api 
+var getCityWeather = function (city) {
     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=0cf1c7e9555bfeee92b38ab834c53129`
 
     fetch(apiURL)
-    .then(function(response){
-        response.json().then(function(data){
+        .then(function (response) {
+            response.json().then(function (data) {
+            });
         });
-    });
 };
+
+//gather weather data from api and display on page
+var displayWeather = function(weather, searchCity) {
+    weatherContainerEl.textContent = '';
+    citySearchInputEl.textContent = searchCity;
+
+    var temperatureEl = document.createElement('span');
+    temperatureEl.textContent = 'temperature: ' + weather.main.temp + ' F';
+
+    var humidityEl = document.createElement('span');
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + ' %';
+
+    var windSpeedEl = document.createElement('span');
+    windSpeedEl.textContent = 'Wind speed: ' + weather.main.speed + ' MPH';
+ }
 
